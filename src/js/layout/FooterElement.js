b16f99b94c70a5d39b8e1b05108cdbb559d627a5
Rory <rgrignard@gmail.com>
2021-11-19 15:12:07 +0000

    Footer - added animated linear gradient hover to icons

--- src/js/layout/FooterElement.js
+++ src/js/layout/FooterElement.js
@@ -1,11 +1,4 @@
 import React from "react";
-import { FaLinkedin} from "react-icons/fa";
-import { FaFacebook} from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { IoLogoWhatsapp } from "react-icons/io";
-import { SiGmail } from "react-icons/si";
-import { FaGithubAlt } from "react-icons/fa";
-import { FaCodepen } from "react-icons/fa";
 import "../../scss/layout/_footer.scss"
 
 function FooterElement() {
@@ -17,26 +10,26 @@ function FooterElement() {
                         <h4>Let's get connected</h4>
                         <ul className={"footer__list"}>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn"} href="https://www.linkedin.com/in/rory-grignard-b3207b66/" target="_blank" rel="noreferrer">
-                                    <FaLinkedin className={"footer__icon"} />
+                                <a className={"footer__btn -linkedin"} href="https://www.linkedin.com/in/rory-grignard-b3207b66/" target="_blank" rel="noreferrer">
+                                    <div className={"footer__icon -animated-hover"}></div>
                                     LinkedIn
                                 </a>
                             </li>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn"} href="https://twitter.com/RoryGrenade" target="_blank" rel="noreferrer">
-                                    <FaTwitter className={"footer__icon"} />
+                                <a className={"footer__btn -twitter"} href="https://twitter.com/RoryGrenade" target="_blank" rel="noreferrer">
+                                    <div className={"footer__icon -animated-hover"}></div>
                                     Twitter
                                 </a>
                             </li>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn"} href="https://codepen.io/RoryGrenade" target="_blank" rel="noreferrer">
-                                    <FaCodepen className={"footer__icon"} />
-                                    CodePen
+                                <a className={"footer__btn -codepen"} href="https://codepen.io/RoryGrenade" target="_blank" rel="noreferrer">
+                                    <div className={"footer__icon -animated-hover"}></div>
+                                    Codepen
                                 </a>
                             </li>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn"} href="https://github.com/RoryGrenade" target="_blank" rel="noreferrer">
-                                    <FaGithubAlt className={"footer__icon"} />
+                                <a className={"footer__btn -github"} href="https://github.com/RoryGrenade" target="_blank" rel="noreferrer">
+                                    <div className={"footer__icon -animated-hover"}></div>
                                     GitHub
                                 </a>
                             </li>
@@ -46,32 +39,32 @@ function FooterElement() {
                         <h4>Share my portfolio</h4>
                         <ul className={"footer__list"}>
                             <li className={"footer__item"}>
-                                <button className={"footer__btn"}>
-                                    <FaLinkedin className={"footer__icon"} />
+                                <button className={"footer__btn -linkedin"}>
+                                    <div className={"footer__icon -animated-hover"}></div>
                                     LinkedIn
                                 </button>
                             </li>
                             <li className={"footer__item"}>
-                                <button className={"footer__btn"}>
-                                    <SiGmail className={"footer__icon"} />
-                                    Email
+                                <button className={"footer__btn -gmail"}>
+                                    <div className={"footer__icon -animated-hover"}></div>
+                                    Gmail
                                 </button>
                             </li>
                             <li className={"footer__item"}>
-                                <button className={"footer__btn"}>
-                                    <IoLogoWhatsapp className={"footer__icon"} />
+                                <button className={"footer__btn -whatsapp"}>
+                                    <div className={"footer__icon -animated-hover"}></div>
                                     WhatsApp
                                 </button>
                             </li>
                             <li className={"footer__item"}>
-                                <button className={"footer__btn"}>
-                                    <FaFacebook className={"footer__icon"} />
+                                <button className={"footer__btn -facebook"}>
+                                    <div className={"footer__icon -animated-hover"}></div>
                                     Facebook
                                 </button>
                             </li>
                             <li className={"footer__item"}>
-                                <button className={"footer__btn"}>
-                                    <FaTwitter className={"footer__icon"} />
+                                <button className={"footer__btn -twitter"}>
+                                    <div className={"footer__icon -animated-hover"}></div>
                                     Twitter
                                 </button>
                             </li>
