db8b2c1d3ee15bbc9ec2fdccbbe5fee6c6537b98
Rory <rgrignard@gmail.com>
2021-12-19 08:42:37 +0000

    Header updates

--- src/js/layout/HeaderElement.js
+++ src/js/layout/HeaderElement.js
@@ -4,48 +4,48 @@ import { RiMenu3Fill } from "react-icons/ri";
 import { RiCloseFill } from "react-icons/ri";
 
 function HeaderElement() {
+
+    function toggleMobileNav() {
+        const headerEl = document.querySelector('.header');
+        headerEl.classList.toggle('--mobile-nav-visible')
+    }
+
+    const navSections = ['Hello', 'Code', 'Stack', 'UI/UX', 'Contact']
+    const navButtons = []
+
+    for (const navSection of navSections) {
+        navButtons.push(
+            <li className="header__nav-item">
+                <button className={"header__nav-btn"} data-nav={navSection}>
+                    <span className={"header__nav-btn-text "}>{navSection}</span>
+                    <span className={"header__nav-btn-clone"}>{navSection}</span>
+                </button>
+            </li>
+        )
+    }
+
+    //Remember to add --active class
+
     return(
         <header className={"header"}>
             <div className={"header__container"}>
-                <div className={"header__button-wrapper"}>
-                    <button className={"header__logo-button"}>
-                        <h1 className={"header__logo-text"}>&#60;RoryGrignard /&#62;<span className={"header__logo-text-clone"}>&#60;RoryGrignard /&#62;</span></h1>
+                <div className={"header__logo-wrapper"}>
+                    <button className={"header__logo-btn"}>
+                        <h1 className={"header__logo-btn-text"}>&#60;RoryGrignard /&#62;</h1>
                     </button>
-                    <button className={"header__burger"} onClick={toggleMobileNav}>
-                        <RiMenu3Fill className={"header__burger-icon -burger"} />
-                        <RiCloseFill className={"header__burger-icon -close"} />
+                    <button className={"header__burger-btn"} onClick={toggleMobileNav}>
+                        <RiMenu3Fill className={"header__burger-btn-icon --burger"} />
+                        <RiCloseFill className={"header__burger-btn-icon --close"} />
                     </button>
                 </div>
                 <nav className={"header__nav"}>
                     <ul className={"header__nav-list"}>
-                        <li className={"header__nav-item"}>
-                            <button className={"header__nav-button active"} data-nav={"Hello"}><span><i>Hello</i></span></button>
-                        </li>
-                        <li className={"header__nav-item"}>
-                            <button className={"header__nav-button"} data-nav={"Code"}><span><i>Code</i></span></button>
-                        </li>
-                        <li className={"header__nav-item"}>
-                            <button className={"header__nav-button"} data-nav={"Stack"}><span><i>Stack</i></span></button>
-                        </li>
-                        <li className={"header__nav-item"}>
-                            <button className={"header__nav-button"} data-nav={"UI/UX"}><span><i>UI/UX</i></span></button>
-                        </li>
-                        {/*<li className={"header__nav-item"}>*/}
-                        {/*    <button className={"header__nav-button"} data-nav={"UI/UX"}><span><i>CMS</i></span></button>*/}
-                        {/*</li>*/}
-                        <li className={"header__nav-item"}>
-                            <button className={"header__nav-button"} data-nav={"Contact"}><span><i>Contact</i></span></button>
-                        </li>
+                        {navButtons}
                     </ul>
                 </nav>
             </div>
         </header>
     )
-}
-
-function toggleMobileNav() {
-    const headerEl = document.querySelector('.header');
-    headerEl.classList.toggle('-nav-active')
 }
 
 export default HeaderElement;
