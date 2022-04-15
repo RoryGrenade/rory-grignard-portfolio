82bcf055f105e67e04b0b1c1d63a54ee483deb12
Rory <rgrignard@gmail.com>
2022-04-15 18:51:57 +0100

    Form - added state to fields

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,10 +1,30 @@
+import { useState } from "react"
 import Intro from "./Intro";
 import "../../scss/components/_form.scss"
 
 export default function Form(props) {
-    const handleSubmit = (e) => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [tel, setTel] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleSubmit = e => {
         e.preventDefault()
-        console.log(e)
+        console.log(firstName)
+        console.log(lastName)
+        console.log(email)
+        console.log(tel)
+        console.log(message)
+    }
+
+    const handleReset = e => {
+        e.preventDefault()
+        setFirstName("")
+        setLastName("")
+        setEmail("")
+        setTel("")
+        setMessage("")
     }
 
     return (
@@ -13,32 +33,79 @@ export default function Form(props) {
 
                 <Intro id={3} />
 
-                <form className="form__form" onSubmit={e => {handleSubmit(e)}}>
+                <form className="form__form"
+                      onReset={(e) => {handleReset(e)}}
+                      onSubmit={(e) => {handleSubmit(e)}}
+                >
                     <div className="form__fields-wrapper">
                         <div className="form__field-wrapper">
-                            <label htmlFor="firstname">First name</label>
-                            <input type="text" name="firstname" id="firstname"/>
+                            <label
+                                className="form__label"
+                                htmlFor="firstname">First name</label>
+                            <input
+                                className="form__field"
+                                type="text"
+                                id="firstname"
+                                required
+                                value={firstName}
+                                onChange={e => setFirstName(e.target.value)}
+                            />
                         </div>
                         <div className="form__field-wrapper">
-                            <label htmlFor="lastname">Last name</label>
-                            <input type="text" name="lastname" id="lastname"/>
+                            <label
+                                className="form__label"
+                                htmlFor="lastname">Last name</label>
+                            <input
+                                className="form__field"
+                                type="text"
+                                id="lastname"
+                                required
+                                value={lastName}
+                                onChange={e => setLastName(e.target.value)}
+                            />
                         </div>
                     </div>
 
                     <div className="form__fields-wrapper">
                         <div className="form__field-wrapper">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email"/>
+                            <label
+                                className="form__label"
+                                htmlFor="email">Email</label>
+                            <input
+                                className="form__field"
+                                type="email"
+                                id="email"
+                                required
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className="form__field-wrapper">
-                            <label htmlFor="tel">Contact no</label>
-                            <input type="text" name="tel" id="tel"/>
+                            <label
+                                className="form__label"
+                                htmlFor="tel">Contact no</label>
+                            <input
+                                className="form__field"
+                                type="text"
+                                id="tel"
+                                value={tel}
+                                onChange={e => setTel(e.target.value)}
+                            />
                         </div>
                     </div>
 
                     <div className="form__field-wrapper">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows={"8"} />
+                        <label
+                            className="form__label"
+                            htmlFor="message">Message</label>
+                        <textarea
+                            className="form__field"
+                            id="message"
+                            rows="8"
+                            required
+                            value={message}
+                            onChange={e => setMessage(e.target.value)}
+                        />
                     </div>
 
                     <div className="form__fields-wrapper --btn">
