70680dee4a5f5f5ac4b0152c0d52c65b7e4c2ec2
Rory <rgrignard@gmail.com>
2022-04-23 13:55:09 +0100

    Footer - removed twitter link from footer and temporarily hid share buttons

--- src/js/layout/FooterElement.js
+++ src/js/layout/FooterElement.js
@@ -15,9 +15,9 @@ export default function FooterElement() {
                                 </a>
                             </li>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn --twitter"} href="https://twitter.com/RoryGrenade" target="_blank" rel="noreferrer">
+                                <a className={"footer__btn --github"} href="https://github.com/RoryGrignard" target="_blank" rel="noreferrer">
                                     <div className={"footer__icon -animated-hover"}/>
-                                    Twitter
+                                    GitHub
                                 </a>
                             </li>
                             <li className={"footer__item"}>
@@ -26,49 +26,43 @@ export default function FooterElement() {
                                     Codepen
                                 </a>
                             </li>
-                            <li className={"footer__item"}>
-                                <a className={"footer__btn --github"} href="https://github.com/RoryGrenade" target="_blank" rel="noreferrer">
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    GitHub
-                                </a>
-                            </li>
                         </ul>
                     </div>
-                    <div className={"footer__col"}>
-                        <h3 className={"footer__title"}>Share</h3>
-                        <ul className={"footer__list"}>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --linkedin"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    LinkedIn
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --gmail"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    Email
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --whatsapp"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    WhatsApp
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --facebook"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    Facebook
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --twitter"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    Twitter
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
+                    {/*<div className={"footer__col"}>*/}
+                    {/*    <h3 className={"footer__title"}>Share</h3>*/}
+                    {/*    <ul className={"footer__list"}>*/}
+                    {/*        <li className={"footer__item"}>*/}
+                    {/*            <button className={"footer__btn --linkedin"}>*/}
+                    {/*                <div className={"footer__icon -animated-hover"}/>*/}
+                    {/*                LinkedIn*/}
+                    {/*            </button>*/}
+                    {/*        </li>*/}
+                    {/*        <li className={"footer__item"}>*/}
+                    {/*            <button className={"footer__btn --gmail"}>*/}
+                    {/*                <div className={"footer__icon -animated-hover"}/>*/}
+                    {/*                Email*/}
+                    {/*            </button>*/}
+                    {/*        </li>*/}
+                    {/*        <li className={"footer__item"}>*/}
+                    {/*            <button className={"footer__btn --whatsapp"}>*/}
+                    {/*                <div className={"footer__icon -animated-hover"}/>*/}
+                    {/*                WhatsApp*/}
+                    {/*            </button>*/}
+                    {/*        </li>*/}
+                    {/*        <li className={"footer__item"}>*/}
+                    {/*            <button className={"footer__btn --facebook"}>*/}
+                    {/*                <div className={"footer__icon -animated-hover"}/>*/}
+                    {/*                Facebook*/}
+                    {/*            </button>*/}
+                    {/*        </li>*/}
+                    {/*        <li className={"footer__item"}>*/}
+                    {/*            <button className={"footer__btn --twitter"}>*/}
+                    {/*                <div className={"footer__icon -animated-hover"}/>*/}
+                    {/*                Twitter*/}
+                    {/*            </button>*/}
+                    {/*        </li>*/}
+                    {/*    </ul>*/}
+                    {/*</div>*/}
                 </div>
                 <div className={"footer__row"}>
                     <div className={"footer__col"}>
