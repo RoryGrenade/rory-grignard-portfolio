3fe8f0d78678a79ebf45adb6ea355123904f83e9
Rory <rgrignard@gmail.com>
2022-05-05 10:01:06 +0100

    Footer updates

--- src/js/layout/FooterElement.js
+++ src/js/layout/FooterElement.js
@@ -9,57 +9,22 @@ export default function FooterElement() {
                         <h3 className={"footer__title"}>Connect</h3>
                         <ul className={"footer__list"}>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn --linkedin"} href="https://www.linkedin.com/in/rory-grignard-b3207b66/" target="_blank" rel="noreferrer">
+                                <a className={"footer__link --linkedin"} href="https://www.linkedin.com/in/rory-grignard-b3207b66/" target="_blank" rel="noreferrer">
                                     <div className={"footer__icon -animated-hover"}/>
                                     LinkedIn
                                 </a>
                             </li>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn --github"} href="https://github.com/RoryGrignard" target="_blank" rel="noreferrer">
+                                <a className={"footer__link --github"} href="https://github.com/RoryGrignard" target="_blank" rel="noreferrer">
                                     <div className={"footer__icon -animated-hover"}/>
                                     GitHub
                                 </a>
                             </li>
                             <li className={"footer__item"}>
-                                <a className={"footer__btn --codepen"} href="https://codepen.io/RoryGrenade" target="_blank" rel="noreferrer">
+                                <a className={"footer__link --codepen"} href="https://codepen.io/RoryGrenade" target="_blank" rel="noreferrer">
                                     <div className={"footer__icon -animated-hover"}/>
                                     Codepen
                                 </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={"footer__col"}>
-                        <h3 className={"footer__title"}>Share</h3>
-                        <ul className={"footer__list"}>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --linkedin"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    LinkedIn
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --gmail"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    Email
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --whatsapp"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    WhatsApp
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --facebook"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    Facebook
-                                </button>
-                            </li>
-                            <li className={"footer__item"}>
-                                <button className={"footer__btn --twitter"}>
-                                    <div className={"footer__icon -animated-hover"}/>
-                                    Twitter
-                                </button>
                             </li>
                         </ul>
                     </div>
